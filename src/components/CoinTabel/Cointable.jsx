2c2eeb270183c161f7ddb5a924c3980bd8a85e26
GOJOSATORU62 <narendra.lama.tech@gmail.com>
2024-12-08 05:35:37 +1100

    Added the coins interface and teh tabel details

--- src/components/CoinTabel/Cointable.jsx
+++ src/components/CoinTabel/Cointable.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { fecthCoinData } from "../../services/fetchCoinData";
 import { useQuery } from "@tanstack/react-query";
 
@@ -7,16 +7,10 @@ function CoinTable() {
   const { data, isLoading, isError, error } = useQuery({
     queryKey: ["coins", page],
     queryFn: () => fecthCoinData(page, "usd"),
-    retry: 2,
-    retryDelay: 1000,
+    // retry: 2,
+    // retryDelay: 1000,
     cacheTime: 1000 * 60 * 2,
   });
-
-  useEffect(() => {
-    if (data) {
-      console.log(data.data);
-    }
-  }, [data]);
 
   if (isLoading) {
     return <div> Loading....</div>;
@@ -27,10 +21,45 @@ function CoinTable() {
   }
 
   return (
-    <>
-      Coin Table <button onClick={() => setPage(page + 1)}>Click</button>
-      {page}
-    </>
+    <div className="my-5 flex flex-col items-center justify-center gap-5 w-[80vw] mx-auto ">
+      <div className="w-full bg-yellow-400 text-black flex py-4 px-2 font-semibold items-center">
+        {/* {Header of the tabel} */}
+
+        <div className="basis-[35%]">Coin</div>
+
+        <div className="basis-[25%]">Price</div>
+
+        <div className="basis-[20%]">24h change</div>
+
+        <div className="basis-[20%]">Market Cap</div>
+      </div>
+
+      <div className="flex flex-col w-[80vw] mx-auto">
+        {data &&
+          data.map((coin) => {
+            return (
+              <div
+                key={coin.id}
+                className="w-full bg-transparent text-white flex py-4 px-2 font-semibold items-center justify-between"
+              >
+                <div className="flex items-center justify-start gap-3 basis-[35%], ">
+                  <div className="w-[5rem] h-[5rem]">
+                    <img src={coin.image} className="w-full h-full " />
+                  </div>
+                  <div className="flex flex-col">
+                    <div className="text-3xl">{coin.name}</div>
+                    <div className="text-xl">{coin.symbol}</div>
+                  </div>
+                </div>
+
+                <div className="basis-[25%]">{coin.current_price}</div>
+                <div className="basis-[20%]">{coin.price_change_24h}</div>
+                <div className="basis-[20%]">{coin.market_cap}</div>
+              </div>
+            );
+          })}
+      </div>
+    </div>
   );
 }
 
--- src/services/fetchCoinData.js
+++ src/services/fetchCoinData.js
@@ -6,8 +6,8 @@ export async function fecthCoinData(page = 1, currency = "usd") {
     const response = await axiosInstance.get(
       `/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=${perPage}&page=${page}`
     );
-    console.log(response);
-    return response;
+
+    return response.data;
   } catch (error) {
     console.error(error);
     return null;
