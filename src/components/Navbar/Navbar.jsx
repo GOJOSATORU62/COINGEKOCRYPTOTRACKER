dd088368045845b9fcdb7242b2f495e0def83e89
GOJOSATORU62 <narendra.lama.tech@gmail.com>
2024-12-15 16:55:34 +1100

    Added home page redirection form navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,18 @@
 // import { useContext } from "react";
 // import { CurrencyContext } from "../../context/CurrencyContext";
 
+import { useNavigate } from "react-router-dom";
 import currencyStore from "../../state/store";
 
 function Navbar() {
   //   const { setCurrency } = useContext(CurrencyContext);
 
   const { setCurrency } = currencyStore();
+  const navigate = useNavigate();
+
+  function goToHome() {
+    navigate("/");
+  }
 
   return (
     <div className="navbar bg-base-100">
@@ -41,7 +47,7 @@ function Navbar() {
           </ul>
         </div>
       </div>
-      <div className="navbar-center">
+      <div onClick={goToHome} className="navbar-center">
         <a className="btn btn-ghost text-xl">Crypto Tracker</a>
       </div>
       <div className="navbar-end">
